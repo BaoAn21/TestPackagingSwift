f76a62542bb0538da99d4a091c85bc5dd3493dd8
AnTran <cao.an99@gmail.com>
2024-07-06 15:26:57 +0700

    modify

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "ChessBoardSwift",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -15,7 +18,12 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ChessBoardSwift"),
+            name: "ChessBoardSwift",
+            path: "Sources",
+            resources: [
+                .process("Assets")
+            ]
+        ),
         .testTarget(
             name: "ChessBoardSwiftTests",
             dependencies: ["ChessBoardSwift"]),
