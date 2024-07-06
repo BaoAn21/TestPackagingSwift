f5d07e85d95708356c43ed68f7b9e133a5d8698f
AnTran <cao.an99@gmail.com>
2024-07-06 16:06:17 +0700

    dealing with Assets

--- Package.swift
+++ Package.swift
@@ -19,10 +19,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "ChessBoardSwift",
-            path: "Sources",
-            resources: [
-                .process("Assets")
-            ]
+            path: "Sources"
         ),
         .testTarget(
             name: "ChessBoardSwiftTests",
